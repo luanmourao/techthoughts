f6c31a8fcd1191b60c0630e2c5e0ad7e10c7784c
Luã Mourão <luan.gcc@gmail.com>
2021-06-25 19:28:24 -0300

    Implementação da funcionalidade de autenticação sign-in/sign-out no componente SignInButton; utilização do provider de autenticação do Next Auth para exposição do status da session (autenticado/não autenticado) em toda a aplicação

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,28 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
+
+  console.log(session);
   
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#02c95c" />
-        Luã Mourão
+      {session.user.name}
       <FiX color="#A8A8B3" className={styles.closeIcon} />
     </button>
   ) : (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#F1416F" />
       Sign in with Github
