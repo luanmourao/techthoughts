07846d6270b41c8a3f0b320650ca10d4092f65be
Luã Mourão <luan.gcc@gmail.com>
2021-06-30 00:20:06 -0300

    Implementa a função responsável por encaminhar o usuário para a página de pagamento
    A função 'handleSubscribe' faz uma chamada à API Route (POST /subscribe) responsável pela criação do id de customer e checkout session do usuário na plataforma Stripe. Na sequência, utiliza o método 'redirectToCheckout' da SDK do Stripe para encaminhar o usuário para a página de pagamento da inscrição

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,11 +8,36 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    // verificar se o usuário está logado e, se não, encaminhá-lo para o login automático com o Github
+    if (!session) {
+      signIn('github');
+
+      return;
+    }
+
+    // chamada HTTP via axios
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+
+    } catch (error) {
+        alert(error.message);
+    }
+  }
 
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
