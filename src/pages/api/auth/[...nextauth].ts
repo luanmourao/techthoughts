58fabf6eb362898184310b36ffcef6910a472eb5
Luã Mourão <luan.gcc@gmail.com>
2021-07-10 15:19:50 -0300

    Implementa callback 'session' do Next Auth
    Essa callback faz uma busca na API de dados do Fauna para encontrar uma subscription ativa para um usuário, retornando, além dos dados padrão da session, uma propriedade 'activeSubscription' com a subscription ativa do usuário ou null caso o usuário não esteja mais com a subscription ativa. A callback foi implementada para que, com a informação sobre a existência ou não de uma subscription ativa, seja possível controlar o acesso ao post e o acesso à plataforma de pagamentos do Stripe.

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -15,6 +15,43 @@ export default NextAuth({
     signingKey: process.env.JWT_SIGNIN_KEY
   },
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        );
+  
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        };
+      } catch (error) {
+          return {
+            ...session,
+            activeSubscription: null
+          };
+      }
+    }, 
+
     async signIn(user, account, profile) {
       const { email } = user;
 
@@ -44,7 +81,6 @@ export default NextAuth({
   
         return true;
       } catch {
-
           return false;
       }
     }
